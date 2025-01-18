51603e3950a632da3e053f6777eba6ef77d8fa17
Niyonkuru2 <150118511+Niyonkuru2@users.noreply.github.com>
2025-01-18 17:56:39 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express()
 await connectDB()
 
 //middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://transactiontracking-frontend.onrender.com", credentials: true }));
 app.use(express.json())
 //Routes
 app.use('/api/user',userRouter)
