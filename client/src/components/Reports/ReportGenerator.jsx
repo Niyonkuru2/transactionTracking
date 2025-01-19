aab31d8713c0020cf38bf4540b8f60dff9d34aa6
Niyonkuru2 <150118511+Niyonkuru2@users.noreply.github.com>
2025-01-19 08:52:21 +0200

    Update ReportGenerator.jsx

--- client/src/components/Reports/ReportGenerator.jsx
+++ client/src/components/Reports/ReportGenerator.jsx
@@ -34,7 +34,7 @@ const ReportGenerator = ({ transactions }) => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="flex items-center justify-center h-screen">
       <div className="bg-white shadow-md rounded-lg p-8 w-full max-w-2xl">
         <h2 className="text-2xl font-bold mb-6 text-center">
           Enter Time Range To Generate Report
